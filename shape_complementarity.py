b0134aff12bf9a7f6194e5230ecd296e335521e1
whalleyt <whalleyt@cardiff.ac.uk>
2023-02-28 16:41:23 +0000

    begin BSA/ASA

--- shape_complementarity.py
+++ shape_complementarity.py
@@ -5,7 +5,7 @@ def main():
     args =  shape_complementarity.pdb_parser.parse_args()
 
     #set up class for complex
-    complex = shape_complementarity.pdb_parser.Complex(args.infile, args.complex_1, args.complex_2)
+    complex = shape_complementarity.pdb_parser.Complex(args.infile, args.complex_1, args.complex_2, verbose=True)
 
 
 if __name__ == "__main__":
