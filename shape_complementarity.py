df986a0a167d2196a083c3feb671f981768b22e9
Tom <whalleyt@cardiff.ac.uk>
2023-02-28 14:42:25 +0000

    add parser module

---
+++ shape_complementarity.py
@@ -0,0 +1,12 @@
+import shape_complementarity.pdb_parser
+
+
+def main():
+    args =  shape_complementarity.pdb_parser.parse_args()
+
+    #set up class for complex
+    complex = shape_complementarity.pdb_parser.Complex(args.infile, args.complex_1, args.complex_2)
+
+
+if __name__ == "__main__":
+    main()
