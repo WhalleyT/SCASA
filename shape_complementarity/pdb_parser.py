789f3fef5e7977367f3466fc9c73288c76ab9147
whalleyt <whalleyt@cardiff.ac.uk>
2023-02-28 15:25:51 +0000

    linting

--- shape_complementarity/pdb_parser.py
+++ shape_complementarity/pdb_parser.py
@@ -1,6 +1,7 @@
 import argparse
 from pathlib import Path
 
+
 def parse_args():
     """
     Parse the command line arguments
@@ -13,25 +14,33 @@ def parse_args():
                              'multiple chains they must be a single uninterrupted chain e.g. --complex_1 ABC',
                         required=True)
     parser.add_argument('--complex_2', '-C2', dest='complex_2', type=str,
-                        help='Chains of first complex, corresponding to the chains in the PDB file. If not supplied ' 
-                        'the default would be all remaining chains',
+                        help='Chains of first complex, corresponding to the chains in the PDB file. If not supplied '
+                             'the default would be all remaining chains',
                         required=False)
     args = parser.parse_args()
     return args
 
+
 class ChainNotFoundException(Exception):
-    '''Custom exception raised when there are no variants detected
-    '''
+    """
+    Custom exception raised when there are no variants detected
+    """
+
     def __init__(self, arg):
         self.arg = arg
         super().__init__(self.arg)
 
-class Complex():
+
+class Complex:
     """
     Object for defining and holding a PDB complex
+    PDBFile: path to PDB file
+    Complex1: Complex of chains, supplied as a string
+    Complex2: As Complex1, but if left as None, it will be assumed to be all remaining chains in PDBFile - Complex1
+    Verbose: Extra logging
     """
 
-    def __init__(self, PDBFile, Complex1, Complex2=None, verbose = False):
+    def __init__(self, pdb_file, complex_1, complex_2=None, verbose=False):
         self.pdb_ranges = {"ATOM": range(0, 4),
                            "SERIAL": range(6, 11),
                            "ATOM_NAME": range(12, 16),
@@ -48,9 +57,9 @@ class Complex():
                            "SEGMENT": range(72, 76),
                            "ELEMENT": range(76, 78)}
 
-        self.pdb_file = PDBFile
-        self.complex_1 = Complex1
-        self.complex_2 = Complex2
+        self.pdb_file = pdb_file
+        self.complex_1 = complex_1
+        self.complex_2 = complex_2
         self.chains = None
         self.verbose = verbose
 
@@ -62,29 +71,39 @@ class Complex():
             print(f"Complex 1: {''.join(self.complex_1)}")
             print(f"Complex 2: {''.join(self.complex_2)}")
 
-
     def verify_chains(self):
-        if Path(self.pdb_file).is_file() == False:
+        """
+        Function to check PDB file exists and chains are in PDB file; then populate Complex2 if left empty
+        :return: None
+        """
+        if not Path(self.pdb_file).is_file():
             raise FileNotFoundError(f"{self.pdb_file} does not exist.")
 
         self.get_all_chains()
 
-
+        # Check complex 1's chains
         for chain in self.complex_1:
             if chain not in self.chains:
                 raise ChainNotFoundException(f" Chain {chain} in complex_1 was not found in PDB file {self.pdb_file}")
 
+        # and now complex 2
         if self.complex_2 is None:
             self.complex_2 = ""
             for chain in self.chains:
                 if chain not in list(self.complex_1):
-                    self.complex_2 +=chain
+                    self.complex_2 += chain
             else:
                 for chain in self.complex_2:
                     if chain not in self.chains:
-                        raise ChainNotFoundException(f" Chain {chain} in complex_2 was not found in PDB file {self.pdb_file}")
+                        raise ChainNotFoundException(
+                            f" Chain {chain} in complex_2 was not found in PDB file {self.pdb_file}")
 
     def get_column(self, key):
+        """
+        Function to extract columns from PDB file based on space indexing.
+        param key: key for dictionary containing deliminations of columns in POB file
+        :return: list_of_col: list of rows found in that PDB column, based on indexes in pdb_ranges
+        """
         list_of_col = []
 
         with open(self.pdb_file) as f:
@@ -97,12 +116,19 @@ class Complex():
         return list_of_col
 
     def get_all_chains(self):
+        """
+        Get all chains in a PDB file that are unique.
+        :return:
+        """
         chains = set(self.get_column("CHAIN"))
         self.chains = sorted(list(set(chains)))
         if self.verbose:
             print(f"PDB file contains chains: {self.chains}")
 
     def chain_string_to_list(self):
+        """
+        convert complex string to list for consistency after possibly parsing them.
+        :return:
+        """
         self.complex_1 = list(self.complex_1)
         self.complex_2 = list(self.complex_2)
-
