c42956e41111ee652e63581014f85e7b46f224db
Tom <whalleyt@cardiff.ac.uk>
2023-08-01 14:01:15 +0100

    Delaunay trianglation of mesh

--- scasa/shape_complementarity.py
+++ scasa/shape_complementarity.py
@@ -1,10 +1,13 @@
+import math
+import random
+
 import numpy as np
 import scipy.spatial
 
 from scipy.spatial import cKDTree
 from dataclasses import dataclass
 from itertools import compress
-
+from scipy.spatial import Delaunay
 
 @dataclass
 class PDBCoords:
@@ -38,7 +41,7 @@ class ShapeComplementarity:
         if self.verbose:
             print("Getting interfaces")
 
-        resiudes = self.get_column("RESIDUE_NUM")
+        residues = self.get_column("RESIDUE_NUM")
         chain = self.get_column("CHAIN")
         amino_acids = self.get_column("RESIDUE_SEQID")
         atoms = self.get_column("ATOM_NAME")
@@ -58,7 +61,8 @@ class ShapeComplementarity:
 
         complex_1_at = []
         complex_2_at = []
-        for x, y, z, c, r, aa, a in zip(x_coord, y_coord, z_coord, chain, resiudes, amino_acids, atoms):
+        for x, y, z, c, r, aa, a in zip(x_coord, y_coord, z_coord, chain,
+                                        residues, amino_acids, atoms):
             coord = np.array((x, y, z), "f")
             if c in self.complex_1:
                 complex_1_residues.append(r)
@@ -90,7 +94,7 @@ class ShapeComplementarity:
 
     def filter_interface(self, a, b, r):
         """
-        Return atoms only within n Angstrongs
+        Return atoms only within n Angstroms
         :return:
         """
 
@@ -119,6 +123,45 @@ class ShapeComplementarity:
             print(f"Estimated area of complex 1's face is {est.area:.2f}Å\N{SUPERSCRIPT THREE}")
         return est.area
 
+    def create_polygon(self, points):
+        x = points[:, 0]
+        y = points[:, 1]
+        z = points[:, 2]
+        two_d = np.vstack([x, y]).T
+        tri = Delaunay(two_d)
+        simplices = tri.simplices
+        return simplices
+
+
+    def point_inside_triangle(self, v1, v2, v3):
+        #P = (1−a−−√)v1 + (a−−√(1−b))v2 + (ba−−√)
+
+        a = random.random()
+        b = random.random()
+        p = (1- math.sqrt(a)) * v1 + (math.sqrt(a) * (1 - b)) * v2 \
+              + (b * math.sqrt(a)) * v3
+        return p
+
+    def random_point(self, coords, simplexes, n_samp):
+
+        # get random indexes of size n_samp
+        indices = np.random.choice(coords.shape[0], n_samp)
+        #create a simplex matrix from the sample
+        simp = simplexes[indices]
+
+        #for a given simplex, sample a random point
+        random_points = []
+        for s in simp:
+            #extract 3 points of a triangle
+            p1 = coords[s[0]]
+            p2 = coords[s[1]]
+            p3 = coords[s[2]]
+            p = self.point_inside_triangle(p1, p2, p3)
+            random_points.append(p)
+
+        return random_points
+
+
     def sc(self):
         complex1, complex2 = self.create_interface()
         complex1 = self.filter_interface(complex1, complex2, self.distance)
@@ -133,8 +176,16 @@ class ShapeComplementarity:
         a1 = self.estimate_volume(complex1.coords)
         a2 = self.estimate_volume(complex2.coords)
 
-        dot_density_1 = self.density * a1
-        dot_density_2 = self.density * a2
+        simplices_c1 = self.create_polygon(complex1.coords)
+        simplices_c2 = self.create_polygon(complex2.coords)
+
+        n_dots_1 = round(self.density * a1)
+        n_dots_2 = round(self.density * a2)
+
+
+        points_c1 = self.random_point(complex1.coords, simplices_c1, n_dots_1)
+        points_c2 = self.random_point(complex2.coords, simplices_c2, n_dots_2)
+
 
     def get_column(self, param):
         pass
